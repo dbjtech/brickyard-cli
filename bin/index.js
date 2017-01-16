71f169e31ec0bec73f2bef789155f7a51d76683c
XueHua Huang <xuehua.huang@dbjtech.com>
2017-01-16 17:33:56 +0800

    全局优化
    * 修改package.json，升级依赖
    * 修复.eslintrc, 升级Eslint规范
    * 删除无意义的文件，bower.json
    * 优化bin/index.js的代码，优化可读性并兼容上述修改

--- bin/index.js
+++ bin/index.js
@@ -1,61 +1,91 @@
 #!/usr/bin/env node
+
 const yargs = require('yargs')
 const hack = require('../lib/hack.js')
 const brickyard = require('../lib/brickyard')
 const logger = require('../lib/logger')
 
-function cmd_handler(argv, cmd) {
-	if (argv._.length !== 1) {
-		throw new Error('Unique <cmd> is needed')
-	}
-	process.nextTick(() => {
+function cmdHandler(cmd) {
+	return (argv) => {
+		if (argv._.length !== 1) {
+			throw new Error('Unique <cmd> is needed')
+		}
 		hack.gulp(argv, cmd)
-	})
+	}
 }
 
-let av = yargs.usage('$0 <cmd> [args]')
+const argv = yargs
+	.usage('$0 <cmd> [args]')
 	.version()
 	.help('help')
 	.demand(1, '<cmd> is needed')
-
-	.count('verbose')
-	.describe('verbose', 'Log level. 0: INFO, 1: DEBUG, 2: TRACE')
-	.alias('v', 'verbose')
-	.boolean('color')
-	.describe('color', 'Log with color')
-	.boolean('debug')
-	.describe('debug', 'Use debug mode to build a plan')
-	.option('brickyard_modules', {
-		desc: 'Path of brickyard_modules folder',
-		default: './brickyard_modules',
+	.options({
+		verbose: {
+			desc: 'Log level. 0: INFO, 1: DEBUG, 2: TRACE',
+			alias: 'v',
+			global: true,
+		},
+		color: {
+			desc: 'Log with color',
+			global: true,
+		},
+		debug: {
+			desc: 'Use debug mode to build a plan',
+			global: true,
+		},
+		brickyard_modules: {
+			desc: 'Path of brickyard_modules folder',
+			default: './brickyard_modules',
+			global: true,
+		},
+		config: {
+			desc: 'Path of config.js',
+			default: './config.js',
+			global: true,
+		},
+		output: {
+			desc: 'Path of output',
+			alias: 'o',
+			default: './output',
+			global: true,
+		},
+		dir: {
+			desc: 'Path of the brickyard app for run',
+			default: './',
+			global: true,
+		},
 	})
-	.option('config', {
-		desc: 'Path of config.js',
-		default: './config.js',
+	.command({
+		command: 'ls [plan...]',
+		desc: 'Get the plan list of brickyard_modules',
+		handler: cmdHandler('ls'),
 	})
-	.option('output', {
-		desc: 'Path of output',
-		alias: 'o',
-		default: './output',
+	.command({
+		command: 'build <plan...>',
+		desc: 'Build one or more plans',
+		handler: cmdHandler('build'),
 	})
-	.option('dir', {
-		desc: 'Path of the brickyard app for run',
-		default: './',
+	.command({
+		command: 'test <plan...>',
+		desc: 'Test one or more plans',
+		handler: cmdHandler('test'),
 	})
-
-	.command('ls [plan...]', 'Get the plan list of brickyard_modules', hack.noop, (argv) => cmd_handler(argv, 'ls'))
-	.command('build <plan...>', 'Build one or more plans', hack.noop, (argv) => cmd_handler(argv, 'build'))
-	.command('test <plan...>', 'Test one or more plans', hack.noop, (argv) => cmd_handler(argv, 'test'))
-	.command('run [dir]', 'Run a brickyard app', hack.noop, (argv) => cmd_handler(argv, 'run'))
-	.command('create-module <type> <dir> [name]', 'Create a brickyard module with name to the dir', hack.noop, (argv) => cmd_handler(argv, 'init'))
-
+	.command({
+		command: 'run [dir]',
+		desc: 'Run a brickyard app',
+		handler: cmdHandler('run'),
+	})
+	.command({
+		command: 'create-module <type> <dir> [name]',
+		desc: 'Create a brickyard module with name to the dir',
+		handler: cmdHandler('init'),
+	})
 	.strict()
 	.argv
 
-// console.log(av)
-logger.setLevel(av.verbose)
+logger.setLevel(argv.verbose)
 logger.hackConsole()
 hack.require()
-module.require_alias('brickyard/private/argv', av)
+module.require_alias('brickyard/private/argv', argv)
 module.require_alias('brickyard/private/logger', logger)
 module.require_alias('brickyard', brickyard)
