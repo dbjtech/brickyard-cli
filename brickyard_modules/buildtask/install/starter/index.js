e9adeb9865da19d3b6726e821f91471f9f3d2fc4
wuzhanwei <zhanwei.wu@dbjtech.com>
2018-07-24 08:53:35 +0800

    feat: load argv to brickyard.argv in starter

--- brickyard_modules/buildtask/install/starter/index.js
+++ brickyard_modules/buildtask/install/starter/index.js
@@ -1,10 +1,11 @@
 const brickyard = require('brickyard-cli/lib/brickyard')
 const hack = require('brickyard-cli/lib/hack')
+const yargs = require('yargs')
 
 hack.logWithLevel(0) // INFO
 
 brickyard.ensureVersion('4.2.0')
-brickyard.loadRuntime({ dir: __dirname })
+brickyard.loadRuntime(Object.assign({ dir: __dirname }, yargs.argv))
 	.then(() => brickyard.sendSignals('run'))
 	.catch((e) => {
 		console.error(e.stack)
