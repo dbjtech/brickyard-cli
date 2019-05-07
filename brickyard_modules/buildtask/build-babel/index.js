52cdf2982865a461db803933c47247b0914dbc03
XueHua Huang <xuehua.huang@dbjtech.com>
2019-05-07 10:34:34 +0800

    Fix conflict for node@11

--- brickyard_modules/buildtask/build-babel/index.js
+++ brickyard_modules/buildtask/build-babel/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, global-require */
 const gulp = require('gulp')
 const brickyard = require('brickyard')
-const _ = require('lodash')
 
 brickyard.ensureVersion('4.0.0-alpha')
 
@@ -10,38 +10,36 @@ const tasks = {
 	 */
 	babel_build_es6_7_backend: () => {
 		const mergeStream = require('merge-stream')
-		const streams = []
-		_.each(brickyard.modules.backend, (plugin, id) => {
-			const path = []
-			path.push(`${plugin.path}/**/*.{es6,es7}`)
-			path.push('!plugins/**/node_modules/**')
-			path.push('!plugins/**/bower_components/**')
 
-			const stream = gulp.src(path)
+		return mergeStream(Object.keys(brickyard.modules.backend).map((id) => {
+			const plugin = brickyard.modules.backend[id]
+			return gulp
+				.src([
+					`${plugin.path}/**/*.{es6,es7}`,
+					'!plugins/**/node_modules/**',
+					'!plugins/**/bower_components/**',
+				])
 				.pipe(gulp.plugins.babel({
 					presets: ['env', 'stage-2'],
 					plugins: [],
 				}))
 				.pipe(gulp.dest(`${brickyard.dirs.modules}/${plugin.type}/${id}`))
-
-			streams.push(stream)
-		})
-
-		if (streams.length) {
-			return mergeStream(streams)
-		}
-		return null
+		}))
 	},
 	/**
 	 * 将项目前端代码里格式为 *.es6, *.es7的文件利用babel转换成 ES5 compatible
 	 */
-	babel_build_es6_7_frontend: () =>
-		gulp.src(`${brickyard.dirs.tempModules}/**/*.{es6,es7}`)
-			.pipe(gulp.plugins.babel({
-				presets: ['env', 'stage-2'],
+	babel_build_es6_7_frontend: () => {
+		const babel = require('gulp-babel')
+
+		return gulp
+			.src(`${brickyard.dirs.tempModules}/**/*.{es6,es7}`)
+			.pipe(babel({
+				presets: ['@babel/preset-env'],
 				plugins: [],
 			}))
-			.pipe(gulp.dest(brickyard.dirs.tempModules)),
+			.pipe(gulp.dest(brickyard.dirs.tempModules))
+	},
 }
 
 gulp.create_tasks(tasks)
