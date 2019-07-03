fbac26a4a283bc5dad2e6943af867d55999ba060
XueHua Huang <xuehua.huang@dbjtech.com>
2019-07-03 15:46:30 +0800

    add scss-loader and babel-loader

--- brickyard_modules/build/webpack/index.js
+++ brickyard_modules/build/webpack/index.js
@@ -25,15 +25,26 @@ gulp.create_tasks({
 			entry: `${brickyard.dirs.tempModules}/main.js`,
 			output: {
 				path: `${brickyard.dirs.dest}/www`,
-				filename: '[name].[chunkhash].js',
+				filename: '[name].js',
 			},
 			module: {
 				rules: [
-					{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
 					{ test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
 					{ test: /\.svg$/, use: ['svg-inline-loader'] },
 					{ test: /\.(html)$/, use: ['html-loader'] },
+					{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+					{ test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
 					{ test: /\.js$/, exclude: /node_modules/, use: ['ng-annotate-loader'] },
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env'],
+							},
+						},
+					},
 				],
 			},
 			plugins: [
@@ -63,6 +74,7 @@ gulp.create_tasks({
 
 		if (stats.hasErrors()) {
 			console.error(info.errors)
+			throw new Error(info.errors)
 		}
 
 		if (stats.hasWarnings()) {
